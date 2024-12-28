5362ea7aaf9c5b3041f6f05bb3e8092f2f7edf48
Kung Li-Chieh <zjkung1123@gmail.com>
2024-12-28 04:05:34 +0000

    feat: add file upload functionality with multer

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
+const app = express();
 
-var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +12,12 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const upload = multer({ dest: 'uploads/' });
 
+app.post('/api/fileanalyse', upload.single('upfile'),(req, res) => {
+  const file = req.file;
+  res.json({name: file.originalname, type: file.mimetype, size: file.size});
+ });
 
 
 const port = process.env.PORT || 3000;
